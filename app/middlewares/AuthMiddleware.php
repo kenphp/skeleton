9647e4f94a1799c667877dfc0ac70e049ee2eaf6
juliardi <ardi93@gmail.com>
2019-02-06 07:15:35 +0700

    updates auth middleware

--- app/middlewares/AuthMiddleware.php
+++ app/middlewares/AuthMiddleware.php
@@ -3,34 +3,28 @@
 namespace app\middlewares;
 
 use Ken\Exception\HttpException;
-
+use Ken\Helpers\Input;
 use Ken\Http\BaseMiddleware;
-
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-
 use Psr\Http\Server\RequestHandlerInterface;
 
+/**
+ * This is only an example of authorization middleware
+ */
 class AuthMiddleware extends BaseMiddleware {
 
     /**
      * @inheritDoc
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
-        $queryParams = $request->getQueryParams();
-        if (isset($queryParams['username']) && $queryParams['username'] == 'admin') {
+        $username = Input::get('username');
+        $password = Input::get('password');
+
+        if ($username == 'admin' && $password == 'pwd') {
             return $this->next($request, $handler);
         }
 
-        $requestHeaders = $request->getHeader('Accept');
-
-        if (in_array('application/json', $requestHeaders)) {
-            return renderJson($this->response, [
-                'code' => 501,
-                'message' => 'Unauthorized',
-            ]);
-        } else {
-            throw new HttpException(501, 'Unauthorized');
-        }
+        throw new HttpException(401, 'Unauthorized');
     }
 }
