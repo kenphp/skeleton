8785fe4ede538792df59ea35725b4316600145f0
juliardi <ardi93@gmail.com>
2019-02-10 15:17:07 +0700

    update composer

--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,15 @@
 <?php
 
+use Ken\Helpers\Log;
 use Ken\Helpers\Route;
 
-Route::get('/', 'HomeController:index');
-Route::get('/json', 'HomeController:json');
+Route::get('/', function ($req, $res, $args = []) {
+    $time = date('Y-m-d H:i:s');
+
+    Log::info('Ken Application loaded');
+
+    return render($res, 'home/index', compact('time'));
+});
 
 Route::group('/api', function () {
     Route::get('/users', 'UserController:list');
